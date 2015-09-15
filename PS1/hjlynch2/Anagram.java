1d18c42c79b93d5e7c6bcbf5d57a750dc7e6e33e
pie3141 <hlconger@gmail.com>
2015-09-15 17:35:42 -0500

    PSET submission

---
+++ PS1/hjlynch2/Anagram.java
@@ -0,0 +1,107 @@
+import java.util.*;
+import java.io.*;
+
+/*
+************************************************
+  ____ ____    _  ___   __   
+ / ___/ ___|  / |/ _ \ / /_  
+| |   \___ \  | | (_) | '_ \ 
+| |___ ___) | | |\__, | (_) |
+ \____|____/  |_|  /_/ \___/ 
+                             
+
+Problem set 1
+
+
+Question 1
+
+A common problem in computer science is finding patterns within data. 
+This problem will simulate that in a way that is easy to see what is happening.
+
+A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward or forward.
+Allowances may be made for adjustments to capital letters, punctuation, and word dividers.
+
+an anagram is a word, phrase, or name formed by rearranging the letters of another.
+
+
+Given a String S, determine if it is an anagram of a palindrome. 
+Return true if the String is an anagram of a palindrome, and false otherwise. 
+For example, the String “oatrtro” will return true (rotator), while the String “false” will return false.
+
+
+PLEASE LOOK AT PS1.txt FOR MORE DETAILS!!!
+
+************************************************
+
+*/
+
+public class Anagram {
+public static boolean anagram(String input) {
+		
+		int length = input.length();
+		char[] letters = new char[length];
+		
+		//insert the String of characters into an array of lowercase characters
+		for(int i=0; i<length; i++){
+			letters[i] = Character.toLowerCase(input.charAt(i));
+		}
+		
+		
+		char temp;
+		char currentChar;
+		
+		//sorts the chars numerically
+		Arrays.sort(letters);
+		
+		int[] nums = new int[length];
+		int numsIndex = 0;
+		char prevChar = 0;
+		
+		//creates an array that contains how many times each char appears 
+		for(int b=0; b<length;b++){
+			if (b==0){
+				nums[numsIndex]++;
+			}
+			else{
+				if(letters[b]==prevChar && nums[numsIndex]<2){
+					nums[numsIndex]++;
+				}
+				else if(letters[b]==prevChar && nums[numsIndex]==2){
+					nums[numsIndex]++;
+				}
+				else{
+					numsIndex++;
+				}
+			}
+			prevChar = letters[b];
+		}
+		
+		int numOfOdds = 0;
+		
+		//if there are more than one set of odd numbers of chars then the given text can't be a palindrome
+		for(int c=0;c<length;c++){
+			if (nums[c]%2!=0){
+				numOfOdds++;
+			}
+			if(numOfOdds>1){
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(anagram("ax-sx"));
+		System.out.println("test");
+		
+		/*
+		 * 4
+		 * aabb
+		 * abba
+		 * xyyxy
+		 * axsx
+		 */
+	}
+
+}
