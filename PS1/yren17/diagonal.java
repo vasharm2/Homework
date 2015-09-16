f446ce796c3044dad6170913e6f7b8fac6bf3ec6
Apple <apple@wirelessprvnat-172-17-113-186.near.illinois.edu>
2015-09-15 20:46:39 -0500

    Changed file name of diagonal.java

--- PS1/yren17/diagonal.java
+++ PS1/yren17/diagonal.java
@@ -1,5 +1,5 @@
 
-public class cs19 {
+public class diagonal {
 	public static void main(String[] args){
 		System.out.println("Please enter the size of your matrix:");
 		int size=TextIO.getlnInt();
