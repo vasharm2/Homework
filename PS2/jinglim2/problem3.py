4bac021b8b39e8f498784d6cd1b8273d02e75376
Phyzer <limjing7@gmail.com>
2015-09-16 18:59:49 -0500

    finished problem3

--- PS2/jinglim2/problem3.py
+++ PS2/jinglim2/problem3.py
@@ -20,13 +20,13 @@ when correct
 
 def printStatement(times, foo):
     length = len(foo);
-    i = 1
+    i = 0
     while i < times:
         j = 0
-        while j <= length:
+        while j < length:
             print(foo[j]),
-        j = j + 1
+            j = j + 1
         print('')
-    i = i + 1
+        i = i + 1
 
 printStatement(3, ['C','S','1','9','6'])
