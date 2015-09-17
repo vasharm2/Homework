269cfcf8299c2b13050446a73bc0603152244eaa
Jordi Paris Ferrer <JParisFerrer@gmail.com>
2015-09-17 09:31:42 -0500

    Problem1 complete, tested on ews (gcc y u do dis 101)

--- PS2/parisfe2/problem1.cpp
+++ PS2/parisfe2/problem1.cpp
@@ -33,16 +33,22 @@ On Windows:
 
 using namespace std;
 
-void printSecondary() {
+void printSecondary() 
+{
     cout << "... but maybe I will make a better one one day?" << endl;
 }
 
-int main() {
-    for (int i = 0; i < 10; i++) {
+int main() 
+{
+    int i = 0;
+    for (i = 0; i < 10; i++) 
+    {
         cout << "I wish I had " << i << " startups." << endl;
-
-        if (i == 3) {
+    
+        if (i == 3) 
+        {
             cout << "I hate my startup." << endl;
             printSecondary();
         }
+    }
 }
