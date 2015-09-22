881f05d2e915b2feae020a794d984078d8c0e26f
Dhruv Bansal <DhruvBansal@wirelessprvnat-172-17-206-233.near.illinois.edu>
2015-09-22 18:15:00 -0500

    Homework 2

--- PS2/dbansal4/problem1.cpp
+++ PS2/dbansal4/problem1.cpp
@@ -5,34 +5,29 @@
 | |   \___ \  | | (_) | '_ \
 | |___ ___) | | |\__, | (_) |
  \____|____/  |_|  /_/ \___/
-
 Problem Set 2
 Problem 1
-
 There are numerous syntactical errors in the code below
 If the code successfully compiles, you are done with this problem!
-
 We suggest you start by compiling and looking at the errors
-
 Compilation instructions:
 C++ is usually compiled with one of 3 programs
 On Mac or Linux:
     try one of 2 commands in the directory of this problem
     1. g++ problem1.cpp
-    2. clang++ problem1.cpp
-
+    2. clang++ problem1.cppq
     if neither works:
     On Mac make sure you install xcode (Mac App Store)
     and install the developer tools
-
     On linux run [your package manager] install clang
     i.e sudo apt-get install clang
-
 On Windows:
     download visual studio express and open this file in that
     and hit the play button to compile
     Or even easier do the hw on ews and follow the clang command
     for linux
+
+author: dbansal4
 ************************************************
 */
 
@@ -41,7 +36,7 @@ On Windows:
 using namespace std;
 
 void printSecondary() {
-    cout << "... but maybe I will make a better one one day?" << endl
+    cout << "... but maybe I will make a better one one day?" << endl;
 }
 
 int main() {
@@ -50,6 +45,7 @@ int main() {
 
         if (i == 3) {
             cout << "I hate my startup." << endl;
-            PrintSecondary;
+            printSecondary();
         }
+    }
 }
--- PS2/dbansal4/problem3.py
+++ PS2/dbansal4/problem3.py
@@ -5,28 +5,28 @@
 | |   \___ \  | | (_) | '_ \
 | |___ ___) | | |\__, | (_) |
  \____|____/  |_|  /_/ \___/
-
 Problem Set 3
 Problem 3
-
 Find the python bug!
 Should print:
 'C S 1 9 6'
 'C S 1 9 6'
 'C S 1 9 6'
 when correct
+
+author: dbansal4
 ************************************************
 """
 
 def printStatement(times, foo):
     length = len(foo);
-    i = 1
+    i = 0
     while i < times:
         j = 0
-        while j <= length:
+        while j < length:
             print(foo[j]),
-        j = j + 1
+            j = j + 1
         print('')
-    i = i + 1
+        i = i + 1
 
 printStatement(3, ['C','S','1','9','6'])
