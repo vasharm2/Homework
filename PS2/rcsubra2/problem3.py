fb98a987544e1faf27ef2ccdf5aa289028c85c75
Rosanna Chen <rchen@Rosannas-MacBook-Pro.local>
2015-09-21 19:24:45 -0500

    PSET2 Submission

---
+++ PS2/rcsubra2/problem3.py
@@ -0,0 +1,35 @@
+"""
+************************************************
+  ____ ____    _  ___   __
+ / ___/ ___|  / |/ _ \ / /_
+| |   \___ \  | | (_) | '_ \
+| |___ ___) | | |\__, | (_) |
+ \____|____/  |_|  /_/ \___/
+
+Problem Set 3
+Problem 3
+
+Find the python bug!
+Should print:
+'C S 1 9 6'
+'C S 1 9 6'
+'C S 1 9 6'
+when correct
+************************************************
+"""
+
+def printStatement(times, foo):
+    length = len(foo);
+    i = 1
+    while i <= times:
+        j = 0
+        while j < length:
+            if(j == length -1):
+                print(foo[j])
+            else:
+                print (foo[j]),
+            j = j + 1
+        i = i + 1
+
+
+printStatement(4, ['C','S','1','9','6'])
