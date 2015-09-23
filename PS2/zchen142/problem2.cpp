dc11d60797692e1e78d4b8286eaa7f01dafd3305
Danielczl <418306719@qq.com>
2015-09-22 23:19:37 -0500

    good

--- PS2/zchen142/problem2.cpp
+++ PS2/zchen142/problem2.cpp
@@ -1,23 +1,20 @@
-//
-//  p2.cpp
-//  C
-//
-//  Created by Daniel Chen on 9/22/15.
-//  Copyright (c) 2015 Daniel. All rights reserved.
-//
+
+  Created by Daniel Chen on 9/22/15.
+  Copyright (c) 2015 Daniel. All rights reserved.
 
 
-//#include <iostream>
-//#include <cmath>
-//
-//using namespace std;
-//
-//int main() {
-//    
-//    for (int i = 0; i < 3; i++) {
-//        for (int j = 3; j > 0; j--) {
-//            cout << pow(i, j) << endl;
-//        }
-//    }
-//    return 0;
-//}
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    
+    for (int i = 0; i < 3; i++) {
+        for (int j = 3; j > 0; j--) {
+            cout << pow(i, j) << endl;
+        }
+    }
+    return 0;
+}
