3fb862f8d83df5286d5ea753dd241456c3c93fbd
atakaha2 <atakaha2@illinois.edu>
2015-09-22 21:34:42 -0500

    Update problem3.py

--- PS2/atakaha2/problem3.py
+++ PS2/atakaha2/problem3.py
@@ -19,17 +19,16 @@ when correct
 """
 
 def printStatement(times, foo):
-  length = len(foo)
+    length = len(foo)
     i = 0
+    j = 1
     while i < times:
-        j = 1
-        print "\'"
-        while j <= length:
-            print(foo[j-1]),
-            print(' '),
-        j = j + 1
-        print "\'"
-    i = i + 1
-
-
+     print('\''),
+     while j <= length:
+       print(foo[j-1]),
+       print(''),
+       j = j + 1
+     print('\'')
+     i = i + 1
+     j = 1
 printStatement(3, ['C','S','1','9','6'])
