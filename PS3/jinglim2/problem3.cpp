153fe21561a6d8653e0fc6b77f339a166d18fe18
Phyzer <limjing7@gmail.com>
2015-09-29 17:43:23 -0500

    finished problem 3

--- PS3/jinglim2/problem3.cpp
+++ PS3/jinglim2/problem3.cpp
@@ -6,11 +6,11 @@ using namespace std;
 
 /*
 *
-*  ____ ____    _  ___   __   
-* / ___/ ___|  / |/ _ \ / /_  
-*| |   \___ \  | | (_) | '_ \ 
+*  ____ ____    _  ___   __
+* / ___/ ___|  / |/ _ \ / /_
+*| |   \___ \  | | (_) | '_ \
 *| |___ ___) | | |\__, | (_) |
-* \____|____/  |_|  /_/ \___/ 
+* \____|____/  |_|  /_/ \___/
 *
 *
 *
@@ -21,7 +21,7 @@ using namespace std;
 * (no deleting any of the current code!)
 * DO NOT SIMPLY ASSIGN POINTER1 AND POINTER2 TO THE CORRECT VALUES
 * reference http://www.cplusplus.com/doc/tutorial/pointers/ for additional information on pointers!
-*/	
+*/
 
 
 int main() {
@@ -45,7 +45,7 @@ int main() {
 	*/
 
 
-	swap(address2, address3);
+	swap(pointer2, pointer3);
 	pointer3 = pointer2;
 	*pointer2 = address3;
 
