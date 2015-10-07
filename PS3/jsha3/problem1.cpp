cdc9dccc34fb45d6fd317867b75807f5fc7dffc1
jsha3 <jsha3@illinois.edu>
2015-10-06 23:57:18 -0500

    PS3

--- PS3/jsha3/problem1.cpp
+++ PS3/jsha3/problem1.cpp
@@ -26,11 +26,16 @@ using namespace std;
 
 int main() {
 	variable declarations
-	string *str1 = "This is the first part, ";
-	string temp = "this is the second part, ";
-	string *str2 = temp;
-	string *temp2 = "and this is the third part";
-	string str3 = temp2;
+	string str1;
+	string temp;
+	string *str2;
+	string *temp2;
+	string str3;
+	str1 = "This is the first part, ";
+	temp = "this is the second part, ";
+	str2 = &temp;
+	temp2 = &str3;
+	*temp2 = "and this is the third part";
 
 
 
@@ -42,7 +47,7 @@ int main() {
 
 
 
-	string str6 = str1 + str2 + str3;
+	string str6 = str1 + *str2 + str3;
 	if(str6.compare("This is the first part, this is the second part, and this is the third part") == 0) cout << str6 << endl << "this is the correct output!" << endl;
 	else cout << "this is the incorrect output :(" << endl;
 
