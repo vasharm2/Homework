73577b825a8ca7876b55e634eb63b42a2ec0e9a9
Rishi Singh <ninthparallel@wirelessprvnat-172-16-241-176.near.illinois.edu>
2015-10-06 21:30:52 -0500

    completed

--- PS3/problem1.cpp
+++ PS3/problem1.cpp
@@ -25,11 +25,11 @@ using namespace std;
 
 
 int main() {
-	variable declarations
-	string *str1 = "This is the first part, ";
+	//variable declarations
+	string str1 = "This is the first part, ";
 	string temp = "this is the second part, ";
-	string *str2 = temp;
-	string *temp2 = "and this is the third part";
+	string *str2 = &temp;
+	string temp2 = "and this is the third part";
 	string str3 = temp2;
 
 
@@ -42,8 +42,9 @@ int main() {
 
 
 
-	string str6 = str1 + str2 + str3;
-	if(str6.compare("This is the first part, this is the second part, and this is the third part") == 0) cout << str6 << endl << "this is the correct output!" << endl;
+	string str6 = str1 + *str2 + str3;
+	if(str6.compare("This is the first part, this is the second part, and this is the third part") == 0)
+		cout << str6 << endl << "this is the correct output!" << endl;
 	else cout << "this is the incorrect output :(" << endl;
 
 	return 0;
