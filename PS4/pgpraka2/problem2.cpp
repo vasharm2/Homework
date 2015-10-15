d6d2b6d6946f7675966772e5d3c30104fc635ad5
Pranay Prakash <pranay.gp@gmail.com>
2015-10-15 03:45:24 -0500

    Imported PS4 files

---
+++ PS4/pgpraka2/problem2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+void problem2::printQuestion2() {
+	std::cout << "This is the question for problem 2" << std::endl;
+	return;
+}
+
+void problem2::printAnswer2() {
+	std::cout << "This is the answer for problem 2!" << std::endl;
+}
