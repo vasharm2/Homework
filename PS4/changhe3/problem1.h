362f1abff0be0c5582e1bc8eb049234a06255840
Chang He <changhe3@illinois.edu>
2015-10-16 17:37:01 -0500

    Submission

---
+++ PS4/changhe3/problem1.h
@@ -0,0 +1,13 @@
+//
+// Created by charlie on 10/16/15.
+//
+
+#ifndef PS4_PROBLEM1_H
+#define PS4_PROBLEM1_H
+class problem1 {
+public:
+    void printQuestion1();
+    void printAnswer1();
+};
+
+#endif //PS4_PROBLEM1_H
