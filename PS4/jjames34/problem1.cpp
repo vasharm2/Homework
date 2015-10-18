e7d4e535a72e7b3b033c804fd9f5a883afbb591a
Jerry James <James_Jerry1@yahoo.com>
2015-10-18 15:59:50 -0500

    PSET 4 Submission

---
+++ PS4/jjames34/problem1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "problem1.h"
+/* The include statement acts as copy/pasting the header file
+(which includes the class definition) into the main.cpp file
+*/
+void problem1::printQuestion1() {
+	std::cout << "This is the question for problem 1" << std::endl;
+	return;
+}
+
+void problem1::printAnswer1() {
+	std::cout << "This is the answer for problem 1!" << std::endl;
+}
