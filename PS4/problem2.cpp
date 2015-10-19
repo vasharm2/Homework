9de446e5a3cad7132567670f8684e6c9d4b8b9cd
ebber <T4lkthr33>
2015-10-19 14:33:00 -0500

    DO NOT COMPILE WITH GCC 5.2.0 ON OSX

--- PS4/problem2.cpp
+++ PS4/problem2.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include "problem2.h"
 
-void printQuestion2() {
+void problem2::printQuestion2() {
 	std::cout << "This is the question for problem 2" << std::endl;
 	return;
 }
 
-void printAnswer2() {
+void problem2::printAnswer2() {
 	std::cout << "This is the answer for problem 2!" << std::endl;
 }
