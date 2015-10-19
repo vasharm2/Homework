3bad5f103929ca1087dd7002bfa3448a113ecc1d
ebber <T4lkthr33>
2015-10-18 23:51:48 -0500

    first attempt at PS4, running into major problems

---
+++ PS4/problem2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "problem2.h"
+
+void printQuestion2() {
+	std::cout << "This is the question for problem 2" << std::endl;
+	return;
+}
+
+void printAnswer2() {
+	std::cout << "This is the answer for problem 2!" << std::endl;
+}
