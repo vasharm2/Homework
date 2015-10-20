bc976efa0d408af8dc7b76ce5291a10db83e754f
atabrah2 <atabrah2@illinois.edu>
2015-10-20 15:08:26 -0500

    Revert "PSET Submission"
    This reverts commit 79ddfeabe0157730c32f55e88c867837e5e9453f.

--- PS4/atabrah2/problem1.h
+++
@@ -1,9 +0,0 @@
-#ifndef PROBLEM1_H
-#define PROBLEM1_H 
-
-class problem1 {
-public:
-	void printQuestion1();
-	void printAnswer1();
-};
-#endif
--- PS4/atabrah2/problem2.h
+++
@@ -1,9 +0,0 @@
-#ifndef PROBLEM2_H
-#define PROBLEM2_H
-
-class problem2 {
-public:
-	void printQuestion2();
-	void printAnswer2();
-};
-#endif
