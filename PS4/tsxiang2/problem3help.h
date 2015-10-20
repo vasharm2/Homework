0cec4f80d8aeb2e79f8784a9b15c44669b253ef2
txiang2 <timothy.xiang@gmail.com>
2015-10-19 21:54:52 -0500

    PSET submission

---
+++ PS4/tsxiang2/problem3help.h
@@ -0,0 +1,19 @@
+#ifndef PROBLEM3
+#define PROBLEM3
+#include <string>
+
+
+class problem3help {
+	public:
+		std::string printQuestion();
+		std::string printAnswer();
+};
+
+std::string problem3help::printQuestion() {
+	return "This is the question for problem 3";
+}
+
+std::string problem3help::printAnswer() {
+	return "This is the answer for problem 3!";
+}
+#endif // PROBLEM3
