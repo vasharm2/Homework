65886782bca91a5ea8aa8f6ccd8eff71791545ca
aneeshdurg <durg2@illinois.edu>
2015-10-21 00:05:44 -0500

    PSET submission

---
+++ PS4/durg2/problem3help.h
@@ -0,0 +1,20 @@
+
+#include <string>
+#ifndef PROBLEM3HELP_H
+#define PROBLEM3HELP_H
+
+class problem3help {
+	public:
+		std::string printQuestion();
+		std::string printAnswer();
+};
+
+std::string problem3help::printQuestion() {
+	return "This is the question for problem 3";
+}
+
+std::string problem3help::printAnswer() {
+	return "This is the answer for problem 3!";
+}
+
+#endif
