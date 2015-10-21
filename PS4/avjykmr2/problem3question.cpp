a40dc6e6725f1661b98005fdb3aa995711e03673
avr914 <arvindvjkumar@gmail.com>
2015-10-21 00:00:27 -0500

    Hi. KTHXBAI. Seriously tho, I'm getting an x86-64 architecture error. idk why. All the syntax seems correct.

---
+++ PS4/avjykmr2/problem3question.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "problem3question.h"
+
+void problem3question::printQuestion3() {
+	problem3help a;
+	std::cout << a.printQuestion() << std::endl;
+}
---
+++ PS4/avjykmr2/problem3question.h
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "problem3help.h"
+
+class problem3question {
+public:
+	void printQuestion3();
+};
